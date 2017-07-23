d9b8343b61603ef3124b7439316f092bb59884d2
ykadosh <yoavks@gmail.com>
2017-07-23 22:16:04 +0300

    Improved x-axis labeling, added comments

--- components/slider/script.js
+++ components/slider/script.js
@@ -27,6 +27,10 @@ Amarkal.UI.registerComponent('slider',{
         $(window).on('resize', this._updateLabelPosition);
         $(window).on('resize', this._populateValueLabels);
     },
+
+    /**
+     * Set the component's properties
+     */
     _setProps: function() {
         var $range = this.$el.find('input'),
             min = parseFloat($range.attr('min')),
@@ -38,9 +42,15 @@ Amarkal.UI.registerComponent('slider',{
             max: max,
             step: step,
             delta: Math.abs(max-min),
-            knobWidth: 26
+            knobWidth: 26,
+            labelWidth: 30, // The width of each label in the horizontal axis, in pixels (must match the CSS)
+            minSpacing: 50 // Minimum spacing between labels
         };
     },
+
+    /**
+     * Reposition the label above the slider's thumb
+     */
     _updateLabelPosition: function() {
         var $range = this.$el.find('input'),
             p = this.props,
@@ -51,9 +61,17 @@ Amarkal.UI.registerComponent('slider',{
         this.$el.find('.slider-value-label')
                 .css({left: left+'px'});
     },
+    
+    /**
+     * Update the value in the label above the thumb
+     */
     _updateLabelValue: function(v) {
         this.$el.find('.slider-value-label').text(v);
     },
+
+    /**
+     * Populate the values along the horizontal axis below the slider
+     */
     _populateValueLabels: function() {
         var $range = this.$el.find('input'),
             $labels = this.$el.find('.slider-values'),
@@ -63,7 +81,7 @@ Amarkal.UI.registerComponent('slider',{
             p = this.props,
             v = $range.val(),
             w = $range.width(),
-            c = Math.floor(Math.min((w+ms)/(lw+ms),p.delta));
+            c = Math.floor(Math.min((w+ms)/(lw+ms),(p.delta/p.step)+1));
         
         $labels.html((function(){
             var html = '';
@@ -74,10 +92,16 @@ Amarkal.UI.registerComponent('slider',{
             return html;
         })());
     },
+
+    /**
+     * Format the given number, adding K/M when applicable
+     */
     _formatNumber: function(n) {
-        var d = this.props.delta;
-        if(d/1000 > 1) return (n/1000).toFixed(1).replace(/\.0$/, "")+'K';
+        var d = this.props.delta,
+            s = this.props.step;
         if(d/1000000 > 1) return (n/1000000).toFixed(1).replace(/\.0$/, "")+'M';
+        if(d/1000 > 1) return (n/1000).toFixed(1).replace(/\.0$/, "")+'K';
+        if(s < 1) return n.toFixed(1).replace(/\.0$/, "");
         return Math.round(n);
     }
 });
